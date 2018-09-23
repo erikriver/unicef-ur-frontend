e5aa4db77a6c4ddfcd5258683ffd7a759fb4ded2
Erik Rivera <erik.river@gmail.com>
2018-09-23 02:52:55 -0500

    wizard

---
+++ src/views/Forms/WizardSteps/Step1.jsx
@@ -0,0 +1,210 @@
+import React from "react";
+
+// @material-ui/icons
+import Face from "@material-ui/icons/Face";
+import RecordVoiceOver from "@material-ui/icons/RecordVoiceOver";
+import Email from "@material-ui/icons/Email";
+
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+
+// core components
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+import PictureUpload from "components/CustomUpload/PictureUpload.jsx";
+import CustomInput from "components/CustomInput/CustomInput.jsx";
+
+const style = {
+  infoText: {
+    fontWeight: "300",
+    margin: "10px 0 30px",
+    textAlign: "center"
+  },
+  inputAdornmentIcon: {
+    color: "#555"
+  },
+  inputAdornment: {
+    position: "relative"
+  }
+};
+
+class Step1 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstname: "",
+      firstnameState: "",
+      lastname: "",
+      lastnameState: "",
+      email: "",
+      emailState: ""
+    };
+  }
+  sendState() {
+    return this.state;
+  }
+  // function that returns true if value is email, false otherwise
+  verifyEmail(value) {
+    var emailRex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (emailRex.test(value)) {
+      return true;
+    }
+    return false;
+  }
+  // function that verifies if a string has a given length or not
+  verifyLength(value, length) {
+    if (value.length >= length) {
+      return true;
+    }
+    return false;
+  }
+  change(event, stateName, type, stateNameEqualTo) {
+    switch (type) {
+      case "length":
+        if (this.verifyLength(event.target.value, stateNameEqualTo)) {
+          this.setState({ [stateName + "State"]: "success" });
+        } else {
+          this.setState({ [stateName + "State"]: "error" });
+        }
+        break;
+      default:
+        break;
+    }
+    this.setState({ [stateName]: event.target.value });
+  }
+  isValidated() {
+    if (
+      this.state.firstnameState === "success" &&
+      this.state.lastnameState === "success"
+    ) {
+      return true;
+    } else {
+      if (this.state.firstnameState !== "success") {
+        this.setState({ firstnameState: "error" });
+      }
+      if (this.state.lastnameState !== "success") {
+        this.setState({ lastnameState: "error" });
+      }
+    }
+    return false;
+  }
+  render() {
+    const { classes } = this.props;
+    return (
+      <GridContainer justify="center">
+        <GridItem xs={12} sm={12}>
+          <h4 className={classes.infoText}>
+            Let's start with the basic information (with validation)
+          </h4>
+        </GridItem>
+        <GridItem xs={12} sm={4}>
+          <PictureUpload />
+        </GridItem>
+        <GridItem xs={12} sm={6}>
+          <CustomInput
+            success={this.state.firstnameState === "success"}
+            error={this.state.firstnameState === "error"}
+            labelText={
+              <span>
+                First Name <small>(required)</small>
+              </span>
+            }
+            id="firstname"
+            formControlProps={{
+              fullWidth: true
+            }}
+            inputProps={{
+              onChange: event => this.change(event, "firstname", "length", 3),
+              endAdornment: (
+                <InputAdornment
+                  position="end"
+                  className={classes.inputAdornment}
+                >
+                  <Face className={classes.inputAdornmentIcon} />
+                </InputAdornment>
+              )
+            }}
+          />
+          <CustomInput
+            success={this.state.lastnameState === "success"}
+            error={this.state.lastnameState === "error"}
+            labelText={
+              <span>
+                Last Name <small>(required)</small>
+              </span>
+            }
+            id="lastname"
+            formControlProps={{
+              fullWidth: true
+            }}
+            inputProps={{
+              onChange: event => this.change(event, "lastname", "length", 3),
+              endAdornment: (
+                <InputAdornment
+                  position="end"
+                  className={classes.inputAdornment}
+                >
+                  <RecordVoiceOver className={classes.inputAdornmentIcon} />
+                </InputAdornment>
+              )
+            }}
+          />
+
+          <FormControl fullWidth className={classes.selectFormControl}>
+            <InputLabel htmlFor="simple-select" className={classes.selectLabel}>
+              Sex
+            </InputLabel>
+            <Select
+              MenuProps={{
+                className: classes.selectMenu
+              }}
+              classes={{
+                select: classes.select
+              }}
+              value={this.state.simpleSelect}
+              onChange={this.handleSimple}
+              inputProps={{
+                name: "simpleSelect",
+                id: "simple-select"
+              }}
+            >
+              <MenuItem
+                disabled
+                classes={{
+                  root: classes.selectMenuItem
+                }}
+              >
+                Sex
+              </MenuItem>
+              <MenuItem
+                classes={{
+                  root: classes.selectMenuItem,
+                  selected: classes.selectMenuItemSelected
+                }}
+                value="2"
+              >
+                Male
+              </MenuItem>
+              <MenuItem
+                classes={{
+                  root: classes.selectMenuItem,
+                  selected: classes.selectMenuItemSelected
+                }}
+                value="3"
+              >
+                Female
+              </MenuItem>
+            </Select>
+          </FormControl>
+        </GridItem>
+      </GridContainer>
+    );
+  }
+}
+
+export default withStyles(style)(Step1);
---
+++ src/views/Forms/WizardSteps/Step2.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import Checkbox from "@material-ui/core/Checkbox";
+import Datetime from "react-datetime";
+
+// core components
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+
+import customSelectStyle from "assets/jss/material-dashboard-pro-react/customSelectStyle.jsx";
+import customCheckboxRadioSwitch from "assets/jss/material-dashboard-pro-react/customCheckboxRadioSwitch.jsx";
+
+const style = {
+  infoText: {
+    fontWeight: "300",
+    margin: "10px 0 30px",
+    textAlign: "center"
+  },
+  inputAdornmentIcon: {
+    color: "#555"
+  },
+  choiche: {
+    textAlign: "center",
+    cursor: "pointer",
+    marginTop: "20px"
+  },
+  ...customSelectStyle,
+  ...customCheckboxRadioSwitch
+};
+
+class Step2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      simpleSelect: "",
+      desgin: false,
+      code: false,
+      develop: false
+    };
+  }
+  sendState() {
+    return this.state;
+  }
+  handleSimple = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.checked });
+  };
+  isValidated() {
+    return true;
+  }
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <h4 className={classes.infoText}>What are you doing? (checkboxes)</h4>
+                <InputLabel className={classes.label}>
+                  Datetime Picker
+                </InputLabel>
+                <br />
+                <FormControl fullWidth>
+                  <Datetime
+                    inputProps={{ placeholder: "Datetime Picker Here" }}
+                  />
+                </FormControl>
+      </div>
+    );
+  }
+}
+
+export default withStyles(style)(Step2);
