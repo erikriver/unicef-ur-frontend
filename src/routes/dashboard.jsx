95a9ae9ae6eb1c4eb248b9fc858fcdc89b6e3483
Daniel <daniel@kimetrics.com>
2018-09-23 11:58:59 -0500

    Last commit

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -1,41 +1,49 @@
 // @material-ui/icons
-import Dashboard from "@material-ui/icons/Dashboard";
+import Dashboard from '@material-ui/icons/Dashboard';
 // import Person from "@material-ui/icons/Person";
 // import ContentPaste from "@material-ui/icons/ContentPaste";
-import LibraryBooks from "@material-ui/icons/LibraryBooks";
-import Notifications from "@material-ui/icons/Notifications";
+import LibraryBooks from '@material-ui/icons/LibraryBooks';
+import Notifications from '@material-ui/icons/Notifications';
 
 // core components/views
-import DashboardPage from "views/Dashboard/Dashboard.jsx";
-import NotificationsPage from "views/Notifications/Notifications.jsx";
-import ApplicantsList from "../views/Applicants/ApplicantsList.jsx";
-import WizardView from "views/Forms/WizardView.jsx";
-import CheckerView from "../views/Checker/CheckerView";
-import RegisterView from "views/Registers/index.js"
+import DashboardPage from 'views/Dashboard/Dashboard.jsx';
+import NotificationsPage from 'views/Notifications/Notifications.jsx';
+import ApplicantsList from '../views/Applicants/ApplicantsList.jsx';
+import WizardView from 'views/Forms/WizardView.jsx';
+import CheckerView from '../views/Checker/CheckerView';
+import RegisterView from 'views/Registers/index.js';
+import HomeView from '../views/Splash/HomeView';
 
 const dashboardRoutes = [
   {
-    path: "/dashboard",
-    sidebarName: "Dashboard",
-    navbarName: "Resume Dashboard",
+    path: '/dashboard',
+    sidebarName: 'Dashboard',
+    navbarName: 'Resume Dashboard',
     icon: Dashboard,
     component: DashboardPage
   },
   {
-    path: "/applicants",
-    sidebarName: "Applicants",
-    navbarName: "Applicants",
+    path: '/applicants',
+    sidebarName: 'Applicants',
+    navbarName: 'Applicants',
     icon: LibraryBooks,
     component: ApplicantsList
   },
   {
-    path: "/applicants/:id",
+    path: '/home',
+    sidebarName: 'Home',
+    navbarName: 'Home',
+    icon: LibraryBooks,
+    component: HomeView
+  },
+  {
+    path: '/applicants/:id',
     component: WizardView
   },
   {
-    path: "/wizard",
-    sidebarName: "Register",
-    navbarName: "Register",
+    path: '/wizard',
+    sidebarName: 'Register',
+    navbarName: 'Register',
     icon: LibraryBooks,
     component: WizardView
   },
@@ -47,9 +55,9 @@ const dashboardRoutes = [
     component: RegisterView
   },
   {
-    path: "/checker",
-    sidebarName: "Checker",
-    navbarName: "Checker",
+    path: '/checker',
+    sidebarName: 'Checker',
+    navbarName: 'Checker',
     icon: LibraryBooks,
     component: CheckerView
   }
---
+++ src/views/Splash/HomeView.jsx
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import withStyles from '@material-ui/core/styles/withStyles';
+
+import dashboardStyle from 'assets/jss/material-dashboard-react/views/dashboardStyle.jsx';
+
+import Icon from '@material-ui/core/Icon';
+// @material-ui/icons
+import Warning from '@material-ui/icons/Warning';
+import DateRange from '@material-ui/icons/DateRange';
+import Accessibility from '@material-ui/icons/Accessibility';
+// core components
+import GridItem from 'components/Grid/GridItem.jsx';
+import GridContainer from 'components/Grid/GridContainer.jsx';
+import Danger from 'components/Typography/Danger.jsx';
+import Card from 'components/Card/Card.jsx';
+import CardHeader from 'components/Card/CardHeader.jsx';
+import CardIcon from 'components/Card/CardIcon.jsx';
+import CardFooter from 'components/Card/CardFooter.jsx';
+
+class HomeView extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {}
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <GridContainer className={classes.background}>
+        <GridItem xs={12} sm={12} md={12} onClick={this.handleClick}>
+          <Card>
+            <CardHeader color="warning" stats icon>
+              <CardIcon color="warning">
+                <Icon />
+              </CardIcon>
+              <h3 className={classes.cardTitle}>Personal Info</h3>
+            </CardHeader>
+            <CardFooter stats>
+              <div className={classes.stats}>
+                <Icon />
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={12} md={12}>
+          <Card>
+            <CardHeader color="danger" stats icon>
+              <CardIcon color="danger">
+                <Icon />
+              </CardIcon>
+              <p className={classes.cardCategory} />
+              <h3 className={classes.cardTitle}>Medical Info</h3>
+            </CardHeader>
+            <CardFooter stats>
+              <div className={classes.stats}>
+                <Icon />
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={12} md={12}>
+          <Card>
+            <CardHeader color="success" stats icon>
+              <CardIcon color="success">
+                <Icon />
+              </CardIcon>
+              <h3 className={classes.cardTitle}>Cultural Info</h3>
+            </CardHeader>
+            <CardFooter stats>
+              <div className={classes.stats}>
+                <Icon />
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+      </GridContainer>
+    );
+  }
+}
+
+HomeView.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(dashboardStyle)(HomeView);
